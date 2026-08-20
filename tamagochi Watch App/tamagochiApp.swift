5389afdd51a9c235ecade3972315cdc625787f48
ajtc16 <ajtc16@gmail.com>
2026-08-20 08:42:02 -0500

    Initial Commit

---
+++ tamagochi Watch App/tamagochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tamagochiApp.swift
+//  tamagochi Watch App
+//
+//  Created by Antonio Teran on 20/8/26.
+//
+
+import SwiftUI
+
+@main
+struct tamagochi_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
